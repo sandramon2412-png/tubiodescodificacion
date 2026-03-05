e127cd5f92acedee8847050125517736320e9903
sandramon2412-png <sandramon2412@gmail.com>
2026-03-04 19:01:03 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("No se pudo encontrar el elemento root para montar la aplicación");
