d657bf5f9e1ae809f735ce328477086063b67057
sandramon2412-png <sandramon2412@gmail.com>
2026-03-04 18:23:21 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
 import ThankYou from './ThankYou';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Meta Pixel
+    (function(f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    (window as any).fbq('init', '1461669411975647');
+    (window as any).fbq('track', 'PageView');
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/gracias" element={<ThankYou />} />
-        {/* Fallback to landing if route not found */}
         <Route path="*" element={<Landing />} />
       </Routes>
     </BrowserRouter>
