083f5ad3b207f4b3615dd709b1bad89bfb194b44
sandramon2412-png <sandramon2412@gmail.com>
2026-03-28 21:10:52 -0500

    Update Landing.tsx

--- Landing.tsx
+++ Landing.tsx
@@ -391,7 +391,7 @@ const Landing: React.FC = () => {
                   {bonusItems.map((item, idx) => (
                     <div key={idx} className="w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-2rem)] bg-white rounded-2xl overflow-hidden flex flex-col group transition transform hover:-translate-y-1 shadow-sm border border-yellow-50">
                       <div className="relative aspect-[4/3] w-full flex items-center justify-center">
-                        <img src={item.image} alt={item.title} className="w-full h-full object-contain p-10 transition duration-500 group-hover:scale-105" style={{ mixBlendMode: 'multiply' }} />
+                        <img src={item.image} alt={item.title} className="w-full h-full object-cover transition duration-500 group-hover:scale-105"} />
                         <div className="absolute top-2 right-2 bg-yellow-400 text-black text-[9px] font-black px-2 py-1 rounded-full shadow-md z-20 uppercase tracking-wider">BONUS GRATIS</div>
                       </div>
                       <div className="p-5 flex-1 flex flex-col">
