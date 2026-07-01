7e34a07d3360c1cbb9315f7f5772aef67774743a
sandramon2412-png <sandramon2412@gmail.com>
2026-07-01 14:21:26 -0500

    Update Landing.tsx

--- Landing.tsx
+++ Landing.tsx
@@ -595,7 +595,8 @@ const Landing: React.FC = () => {
                   animate={{ y: [0, -25, 0] }}
                   transition={{ duration: 6, repeat: Infinity }}
                 >
-                 
+                  <img 
+                    src="https://i.imgur.com/tp3ywRK.png" 
                     alt="App Código Cuerpo" 
                     className="w-full rounded-2xl shadow-elevated border-4 border-white/30 object-contain"
                   />
