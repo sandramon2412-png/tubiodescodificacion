ea0797ec45cc6c175e0d47ce2a5de5829c147d4d
sandramon2412-png <sandramon2412@gmail.com>
2026-07-01 14:28:02 -0500

    Update Landing.tsx

--- Landing.tsx
+++ Landing.tsx
@@ -606,7 +606,8 @@ const Landing: React.FC = () => {
                   animate={{ y: [0, 25, 0] }}
                   transition={{ duration: 6, repeat: Infinity, delay: 0.3 }}
                 >
-                  <
+                  <img 
+                    
                     alt="App Preview" 
                     className="w-full rounded-2xl shadow-elevated border-4 border-white/20 object-contain"
                   />
